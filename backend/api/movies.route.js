dd5b1503d056f88e3003b8ca1922dec042323588
onmecode <onmecode@gmail.com>
2025-08-27 11:28:51 +0100

    updated route - created controller and added post and update review

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,5 +1,11 @@
 import express from 'express'
 import MoviesController from '../dao/movies.controller.js'
+import ReviewsController from '../dao/reviews.controller.js'
 const router = express.Router()
 router.route('/').get(MoviesController.apiGetMovies)
+router
+  .route("/review")
+  .post(ReviewsController.apiPostReview)
+  .put(ReviewsController.apiUpdateReview)
+  .delete(ReviewsController.apiDeleteReview)
 export default router
